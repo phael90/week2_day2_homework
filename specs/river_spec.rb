4c706d62d5abbbf6c902253c225515d0bd065ebe
Raphael Ugha <raphaelugha@gmail.com>
2018-07-03 23:31:44 +0100

    passed test_fish_count

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,14 +1,20 @@
 require("minitest/autorun")
 require_relative("../river")
+require_relative("../fish")
 
 class TestRiver < MiniTest::Test
 
   def setup
-    @river = River.new("Kikadu")
+    @fish = Fish.new("Fizzy")
+    @river = River.new("Kikadu", ["Demo", "Nemo", "Lenno"])
   end
 
   def test_name
     assert_equal("Kikadu", @river.name)
+  end
+
+  def test_fish_count
+    assert_equal(3, @river.fish_count)
   end
 
 end
