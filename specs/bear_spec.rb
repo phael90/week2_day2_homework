aeacabf6b730a7bec24d42962a2ddc42eab5031b
Raphael Ugha <raphaelugha@gmail.com>
2018-07-03 22:22:42 +0100

    passed test_food_count

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -3,17 +3,19 @@ require_relative("../bear")
 
 class TestBear < MiniTest::Test
 
-def setup
-  @bear = Bear.new("Wojtek", "Brown")
-end
+  def setup
+    @bear = Bear.new("Wojtek", "Brown")
+  end
 
+  def test_bear_name
+    assert_equal("Wojtek", @bear.name)
+  end
 
-def test_bear_name
-  assert_equal("Wojtek", @bear.name)
-end
+  def test_bear_type
+    assert_equal("Brown", @bear.type)
+  end
 
-def test_bear_type
-  assert_equal("Brown", @bear.type)
-end
-
+  def test_food_count
+    assert_equal(0, @bear.food_count)
+  end
 end
